f56b88931dd485078728bed8306acb52eaa6a3da
Krishna Chatpar <kchatpar@yahoo.com>
2019-12-20 13:02:31 -0500

    added session ID to candidates returned

--- htdocs/api/v0.0.3-dev/CandidatesExtID.php
+++ htdocs/api/v0.0.3-dev/CandidatesExtID.php
@@ -72,12 +72,13 @@ class Candidates extends APIBase
     public function handleGET()
     {
         $candidates = $this->DB->pselect(
-            "SELECT c.CandID, ProjectID, PSCID, s.Alias as Site,
-                    EDC, DoB, Sex, ExtStudyID
+            "SELECT c.CandID, ProjectID, PSCID, s.Name as Site,
+                    EDC, DoB, Sex, ExtStudyID, ses.ID
              FROM candidate c JOIN psc s on (s.CenterID=c.RegistrationCenterID)
                  LEFT JOIN candidate_project_extid_rel cpe ON (cpe.CandID = c.CandID) 
                  LEFT JOIN Project_external pe ON (cpe.ProjectExternalID= pe.ProjectExternalID)
-             WHERE Active='Y' AND pe.Name=:name
+                 LEFT JOIN session ses ON (cpe.CandID = ses.CandID)
+             WHERE c.Active='Y' AND pe.Name=:name
                 ",
             ["name"=>"QPN"]
         );
